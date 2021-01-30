96e4df0f5b4f0c1f74cbc5a3674984fa69e98bee
Nihal Coşkun <nihalcoskkun@gmail.com>
2021-01-30 18:56:58 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] idNumbers;
+        T2[] products;
+
+        T1[] TempId;
+        T2[] TempProduct;
+
+        public MyDictionary()
+        {
+         idNumbers = new T1[0];
+         products = new T2[0];
+    }
+        public void Add(T1 key, T2 value)
+        {
+            TempId = idNumbers;
+            TempProduct = products;
+
+            idNumbers = new T1[idNumbers.Length + 1];
+            products = new T2[products.Length + 1];
+
+            for (int i = 0; i < TempId.Length; i++)
+            {
+                idNumbers[i] = TempId[i];
+                products[i] = TempProduct[i];
+            }
+
+            idNumbers[idNumbers.Length - 1] = key;
+            products[products.Length - 1] = value;
+            Console.WriteLine("Ürün Adı: " + value + " ID:" + key );
+            Console.WriteLine("----------------------------------");
+           
+        }
+
+        public int numOfProducts
+        {
+            get { return products.Length; }
+        }
+        
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        /*Dictionary sınıfı bir koleksiyon sınıfıdır.İçinde belirlediğimiz typeda bir 'key' ve ona karşılık gelen 'değeri' saklar.
+        Arraylerden farklı olarak değişken boyutludur.Dilediğimiz gibi listeleme yapıp, eleman ekleyip-çıkartma gerçekleştirebiliriz.*/
+        static void Main(string[] args)
+        {
+            MyDictionary <int , string> catalog = new MyDictionary<int , string>();
+
+            catalog.Add(150119791, "IKEA Dolap");
+            catalog.Add(886549372, "PANAMA Nevresim");
+            catalog.Add(624753188, "FLASHLIGHT Masa Lambası");
+            catalog.Add(426753147, "DELUXE Koltuk Takımı");
+
+
+            Console.WriteLine("Toplam ürün adedi: " + catalog.numOfProducts);
+
+
+        }
+       
+    }
+
+    
+}
